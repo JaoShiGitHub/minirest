346cbb3d149067a5d7041725a9d8a268acbc6911
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-02-12 00:14:17 +0700

    Update products.js

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,1 +1,24 @@
 import { Router } from "express";
+import { validateLoginCostumer } from "./validateLoginCostumer";
+import { getMenu } from "../controllers/costumer";
+
+const costumer = Router();
+
+costumer.get("/", (req, res) => {
+  res.json({ message: "Home page" });
+});
+
+costumer.get("/menu", getMenu); // ✅
+
+costumer.get("/login", [validateLoginCostumer], (req, res) => {});
+
+costumer.post("/register", (req, res) => {});
+
+costumer.get("/cart", (req, res) => {});
+
+costumer.post("/cart", (req, res) => {});
+costumer.get("/history", (req, res) => {});
+costumer.delete("/history", (req, res) => {});
+
+costumer.put("/currentorders/:id", (req, res) => {});
+costumer.get("/currentorders", (req, res) => {});
