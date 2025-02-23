54587ff656cfbd7f04ef864a0adff82e788e77a8
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-02-23 23:47:09 +0700

    add auth

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import bodyParser from "body-parser";
 import express from "express";
+import authRouter from "./apps/auth.js";
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use("/auth", authRouter);
 
 app.get("/trips", (req, res) => {
   res.send("Attractions Data");
