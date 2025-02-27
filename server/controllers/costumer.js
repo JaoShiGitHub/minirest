a35b27f281d5d6fce191de6fbaf72a13ceeb81cd
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-02-27 23:59:48 +0700

    Update costumer.js

--- server/controllers/costumer.js
+++ server/controllers/costumer.js
@@ -1,12 +1,6 @@
 import { pgGetAllMenu } from "../utils/db";
 
 const getMenu = async (req, res) => {
-  try {
-    const data = await pgGetAllMenu();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
 };
 
 export { getMenu };
