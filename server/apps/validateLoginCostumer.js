55a8edef44625a704ba6a55acca56f723c0fc4c5
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-02 17:35:01 +0700

    Update validation

--- server/apps/validateLoginCostumer.js
+++ server/apps/validateLoginCostumer.js
@@ -1,11 +1,37 @@
-export const validateLoginCostumer = (req, res, next) => {
-  const costumer = req.body;
+const validateLoginCustomer = async (req, res, next) => {
+  const { identifier, password } = req.body;
+  // identifier = email || username
 
-  if (!costumer.email) {
-    return res.status.json({
-      message: "Name is required",
+  if (!identifier) {
+    return res.status(400).json({
+      error: "Please type your username or password.",
     });
   }
 
-  next();
+  if (!password) {
+    return res.status(400).json({ message: "Password is required." });
+  }
+
+  function isEmail(input) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
+  }
+
+  const type = isEmail ? "email" : "username";
+
+  try {
+    const query = `SELECT EXISTS (SELECT 1 FROM customers WHERE ${type} = $1)`;
+    const data = await pool.query(query, [identifier]);
+    if (data.rows && data.rows.length > 0 && data.rows[0].exists) {
+      return next();
+    } else {
+      return res
+        .status(404)
+        .json({ error: `${type} "${identifier}" was not found.` });
+    }
+  } catch (error) {
+    console.error("Database Error: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
+export default validateLoginCustomer;
