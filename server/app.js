cff80b3f48eb7fd848bccae88c47561f17a9478f
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-02 17:33:40 +0700

    getMenu

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 import bodyParser from "body-parser";
 import express from "express";
 import authRouter from "./apps/auth.js";
+import { getMenu } from "./controllers/menu.js";
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use("/auth", authRouter);
+app.get("/menu", getMenu);
 
 app.listen(4000, () => {
   console.log("Server is listening at port 4000");
