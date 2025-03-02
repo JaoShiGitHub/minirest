7516b90335a21e070b6628f3de2127fed558e4b1
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-02 17:39:58 +0700

    Update password

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,7 +2,7 @@ import * as pg from "pg";
 const { Pool } = pg.default;
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:postgres@localhost:5432/posts",
+  connectionString: "postgresql://postgres:081400@localhost:5432/mini_rest",
 });
 
 export { pool };
