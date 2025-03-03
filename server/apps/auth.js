9c3bdead529776f747b88e8df9c6c16c10123426
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-03 14:31:55 +0700

    Update auth.js

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,3 +1,33 @@
-const authRouter = () => {};
+import bcrypt from "bcrypt";
+import { Router } from "express";
+import { pool } from "../utils/db.js";
+
+const authRouter = Router();
+
+authRouter.post("/login", async (req, res) => {
+  const { identifier, password } = req.body;
+  function isEmail(input) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
+  }
+
+  const type = isEmail ? "Email" : "Username";
+
+  const data = await pool.query(
+    `SELECT email, password FROM customers WHERE ${type.toLocaleLowerCase()} = $1`,
+    [identifier]
+  );
+
+  const customer = data.rows[0];
+
+  if (customer.length === 0) {
+    return res.status(404).json({ message: `${type} not found` });
+  }
+
+  const isValidPassword = await bcrypt.compare(password, customer.password);
+
+  if (!isValidPassword) {
+    return res.status(400).json({ message: "Invalid Password" });
+  }
+});
 
 export default authRouter;
