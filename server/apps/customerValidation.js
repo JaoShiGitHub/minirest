bdc6d1b72d590e00f5f3365e782be6995339d963
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-04 12:48:06 +0700

    Create requiredFields

--- server/apps/customerValidation.js
+++ server/apps/customerValidation.js
@@ -19,6 +19,16 @@ const validateRegisterCustomer = async (req, res, next) => {
   const { username, password, firstName, lastName, tel, email, birthday } =
     req.body;
 
+  const requiredFields = {
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    tel: req.body.tel,
+    email: req.body.email,
+    birthday: req.body.birthday,
+  };
+
   if (!username) {
     return res.status(400).json({ message: "Username is required!" });
   } else if (username.length > 20) {
@@ -27,9 +37,16 @@ const validateRegisterCustomer = async (req, res, next) => {
     });
   }
 
-  if (!password) {
-    return res.status(400).json({ message: "Password is required!" });
+  for (const [field, value] of Object.entries(requiredFields)) {
+    if (!value) {
+      return res.status(400).json({
+        message: `${
+          field.charAt(0).toUpperCase() + field.slice(1)
+        } is required!`,
+      });
+    }
   }
+
   next();
 };
 
