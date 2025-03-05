df9e011391c3196807a1ecd509c0e43470ef962d
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-05 11:22:38 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 import bodyParser from "body-parser";
 import express from "express";
-import authRouter from "./apps/auth.js";
-import { getMenu } from "./controllers/menu.js";
 import dotenv from "dotenv";
+
+import authRouter from "./apps/auth.js";
+import menu from "./apps/menu_routers.js";
+import customer from "./apps/customer_routers.js";
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
 app.use("/auth", authRouter);
-app.get("/menu", getMenu);
+app.use("/menu", menu);
+app.use("/customer", customer);
 
 app.listen(4000, () => {
   console.log("Server is listening at port 4000");
