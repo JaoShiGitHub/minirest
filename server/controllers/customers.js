5e6e7a74f7ed503f4b2e2936347d08a089b0a320
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-05 11:44:14 +0700

    Update customers.js

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -1,8 +1,5 @@
-const customerLogin = (req, res) => {
-  return res.json({ message: "Login Successful" });
-};
-
 const customerRegister = (req, res) => {
   return res.json({ message: "Hello register" });
 };
-export { customerLogin, customerRegister };
+
+export { customerRegister };
