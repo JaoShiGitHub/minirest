efbe2406913c7aaf5e5b9cc6b58d8106def33091
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-06 12:42:41 +0700

    Insert info

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -1,5 +1,28 @@
-const customerRegister = (req, res) => {
-  return res.json({ message: "Hello register" });
+import { pool } from "../utils/db.js";
+
+const customerRegister = async (req, res) => {
+  const {
+    username,
+    password,
+    firstName,
+    lastName,
+    tel,
+    email,
+    birthday,
+    allergy,
+  } = req.body;
+
+  try {
+    await pool.query(
+      `INSERT INTO customers (username, password, firstname, lastname, tel, email, allergy, birthday ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+      [username, password, firstName, lastName, tel, email, allergy, birthday]
+    );
+    return res
+      .status(200)
+      .json({ message: "Welcome to the Mini Rest in Thailand ka." });
+  } catch (error) {
+    return res.json({ message: `Error inserting user: ${error}` });
+  }
 };
 
 export { customerRegister };
