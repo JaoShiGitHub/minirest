0c3408b86f2efa02cce3e29ccad2a752bb03c33e
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-06 13:25:33 +0700

    Use Bcrypt for password

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -1,21 +1,27 @@
 import { pool } from "../utils/db.js";
+import bcrypt from "bcrypt";
 
 const customerRegister = async (req, res) => {
-  const {
-    username,
-    password,
-    firstName,
-    lastName,
-    tel,
-    email,
-    birthday,
-    allergy,
-  } = req.body;
+  const { username, firstName, lastName, tel, email, birthday, allergy } =
+    req.body;
+  const customer = { password: req.body.password };
+
+  const salt = await bcrypt.genSalt(10);
+  customer.password = await bcrypt.hash(customer.password, salt);
 
   try {
     await pool.query(
       `INSERT INTO customers (username, password, firstname, lastname, tel, email, allergy, birthday ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-      [username, password, firstName, lastName, tel, email, allergy, birthday]
+      [
+        username,
+        customer.password,
+        firstName,
+        lastName,
+        tel,
+        email,
+        allergy,
+        birthday,
+      ]
     );
     return res
       .status(200)
