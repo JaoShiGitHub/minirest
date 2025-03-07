9f4b574b09e7e3d1b2eddc0327eb15e00b25e209
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-07 21:35:50 +0700

    Add admin router

--- server/app.js
+++ server/app.js
@@ -5,14 +5,16 @@ import dotenv from "dotenv";
 import authRouter from "./apps/auth.js";
 import menu from "./apps/menu_routers.js";
 import customer from "./apps/customer_routers.js";
+import admin from "./apps/admin_routers.js";
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
-app.use("/auth", authRouter);
+app.use("/auth", authRouter); // Login
 app.use("/menu", menu);
 app.use("/customer", customer);
+app.use("/admin", admin);
 
 app.listen(4000, () => {
   console.log("Server is listening at port 4000");
