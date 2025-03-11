4682591c9b33538be856837d2927ae60cb9a3885
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-11 16:50:46 +0700

    Add paths

--- client/src/pages/AuthenticatedApp.jsx
+++ client/src/pages/AuthenticatedApp.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
+import MenuPage from "./MenuPage";
 
 function AuthenticatedApp() {
   return (
     <div className="App">
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </div>
   );
