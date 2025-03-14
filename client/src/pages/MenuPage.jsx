8df164014590416739887f12867411cdc3ebaee3
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-14 15:38:25 +0700

    Create condition to display order form or normal menu list

--- client/src/pages/MenuPage.jsx
+++ client/src/pages/MenuPage.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 function MenuPage() {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [orderStatus, setOrderStatus] = useState(false);
+  const [isChecked, setIsCheck] = useState(false);
 
   useEffect(() => {
     getMenu();
@@ -20,22 +22,48 @@ function MenuPage() {
     }
   };
 
+  const handleOnSubmit = (e) => e.preventDefault();
+  const handleCheckboxChange = (e) => setIsCheck(e.target.checked);
+
   return (
     <div>
       {data.length > 0 ? (
-        <div className="menu-container">
-          {data.map((menu) => {
-            return (
-              <div key={menu.menu_id}>
-                <p>{menu.name}</p>
-                <span>{menu.price}</span>
-              </div>
-            );
-          })}
-        </div>
+        !orderStatus ? (
+          <div className="menu-container">
+            {data.map((menu) => {
+              return (
+                <div key={menu.menu_id}>
+                  <p>{menu.name}</p>
+                  <span>{menu.price}</span>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <form onSubmit={handleOnSubmit}>
+            {data.map((menu) => {
+              return (
+                <label key={menu.menu_id}>
+                  <input
+                    id={menu.menu_id}
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                    name={menu.name}
+                  />
+                  <p>{menu.name}</p>
+                  <span>{menu.price}</span>
+                </label>
+              );
+            })}
+            <button type="submit">SUBMIT</button>
+          </form>
+        )
       ) : null}
       <br />
       <button onClick={() => navigate("/home")}>BACK TO HOME</button>
+      <br />
+      <button onClick={() => setOrderStatus(true)}>ORDER</button>
     </div>
   );
 }
