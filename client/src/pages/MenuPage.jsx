a98a8a500f145e999db42477f9250f0953d49610
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-14 15:34:30 +0700

    Update MenuPage.jsx

--- client/src/pages/MenuPage.jsx
+++ client/src/pages/MenuPage.jsx
@@ -1,5 +1,43 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function MenuPage() {
-  return <div>MenuPage</div>;
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
+  const getMenu = async () => {
+    try {
+      const MENU = await axios.get("http://localhost:4000/menu");
+      setData(MENU?.data?.data?.rows);
+      console.log(MENU?.data?.data?.rows);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      {data.length > 0 ? (
+        <div className="menu-container">
+          {data.map((menu) => {
+            return (
+              <div key={menu.menu_id}>
+                <p>{menu.name}</p>
+                <span>{menu.price}</span>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+      <br />
+      <button onClick={() => navigate("/home")}>BACK TO HOME</button>
+    </div>
+  );
 }
 
 export default MenuPage;
