1f63b8fca0cd2edcd359406bd2d360a7d19d6afa
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-17 16:11:38 +0700

    Add PlaceOrderPage

--- client/src/pages/AuthenticatedApp.jsx
+++ client/src/pages/AuthenticatedApp.jsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import MenuPage from "./MenuPage";
+import NotFoundPage from "./NotFoundPage";
+import PlaceOrderPage from "./PlaceOrderPage";
+import HistoryPage from "./HistoryPage";
 
 function AuthenticatedApp() {
   return (
@@ -8,7 +11,9 @@ function AuthenticatedApp() {
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/menu" element={<MenuPage />} />
-        <Route path="*" element={<HomePage />} />
+        <Route path="/order-now" element={<PlaceOrderPage />} />
+        <Route path="/order-history" element={<HistoryPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
