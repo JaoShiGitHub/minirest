f48f6e73256a3d407442f7cf566edb5e12fab137
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-18 16:17:51 +0700

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
-  const handleMenuBtn = () => {};
+  const navigate = useNavigate();
+
+  const handleMenuBtn = () => {
+    navigate("/menu");
+  };
+
+  const handleHistoryBtn = () => {
+    navigate("/order-history");
+  };
+
   return (
     <div>
       <header>
         <h1>MINIREST</h1>
         <p>Welcome to Minirest</p>
       </header>
-      <button onClick={handleMenuBtn}>MENU</button>
+      <nav>
+        <button onClick={handleMenuBtn}>MENU</button>
+        <button onClick={handleHistoryBtn}>HISTORY</button>
+      </nav>
     </div>
   );
 }
