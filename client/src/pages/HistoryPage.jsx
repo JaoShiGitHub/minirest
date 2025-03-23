c841cc46c949900bcd463088709c755e08c52ab6
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-23 17:49:38 +0700

    Update HistoryPage.jsx

--- client/src/pages/HistoryPage.jsx
+++ client/src/pages/HistoryPage.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function HistoryPage() {
   const [orders, setOrders] = useState([]);
   const navigator = useNavigate();
+  const [deletedItem, setDeletedItem] = useState({ delete: "" });
 
   const style = {
     cursor: "pointer",
@@ -19,6 +20,7 @@ function HistoryPage() {
       const orders = await axios.get(
         "http://localhost:4000/order/history?customer_id=10"
       );
+
       setOrders(orders.data.items);
     } catch (err) {
       alert(err.response.data.msg);
@@ -26,24 +28,28 @@ function HistoryPage() {
   };
 
   const handleOrderClick = (order, key) => {
-    console.log(key);
-
-    navigator("/view-order", { state: { order, key } });
+    navigator("/view-order", {
+      state: { order: order, key: key, setDeletedItem: setDeletedItem },
+    });
   };
+
+  const filterOrders = Object.entries(orders).filter((order) =>
+    deletedItem.delete ? order.order_id !== deletedItem.delete : orders
+  );
 
   return (
     <div>
       <h1>HistoryPage</h1>
-      {Object.entries(orders).map(([key, order]) => {
+      {filterOrders.map((order) => {
         return (
           <button
             style={style}
-            key={order.id}
-            onClick={() => handleOrderClick(order, key)}
+            key={order[0]}
+            onClick={() => handleOrderClick(order[1])}
           >
-            <h1>Order: {key}</h1>
+            <h1>Order: {order[0]}</h1>
             <div>
-              {order.map((item) => {
+              {order[1].map((item) => {
                 return (
                   <div key={item.id}>
                     <div>{item.product_name}</div>
@@ -54,7 +60,7 @@ function HistoryPage() {
             </div>
             <span>
               Total:
-              {order.reduce((orderAcc, item) => {
+              {order[1].reduce((orderAcc, item) => {
                 return orderAcc + parseFloat(item.product_price);
               }, 0)}
             </span>
@@ -65,4 +71,4 @@ function HistoryPage() {
   );
 }
 
-export default HistoryPage;
+export { HistoryPage };
