9e83d7f7aa30adbaf74ea8ce2e282944abdd748b
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-26 17:31:49 +0700

    Add edit-customer-info path

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ import authRouter from "./apps/auth.js";
 import menu from "./apps/menu_routers.js";
 import customer from "./apps/customer_routers.js";
 import admin from "./apps/admin_routers.js";
+import order from "./apps/order_routers.js";
+import { editCustomerInfo } from "./controllers/customers.js";
 
 const app = express();
 dotenv.config();
@@ -17,6 +19,8 @@ app.use("/auth", authRouter); // Login
 app.use("/menu", menu);
 app.use("/customer", customer);
 app.use("/admin", admin);
+app.use("/order", order);
+app.put("edit-customer-info", editCustomerInfo);
 
 app.listen(4000, () => {
   console.log("Server is listening at port 4000");
