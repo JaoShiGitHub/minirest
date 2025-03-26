d463e4e4a9f6a6ace3ad7f3e34e0846d222f8c6c
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-26 15:37:24 +0700

    Store deleted order in the localStorage

--- client/src/pages/ViewOrder.jsx
+++ client/src/pages/ViewOrder.jsx
@@ -1,10 +1,16 @@
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function ViewOrder() {
   const location = useLocation();
+  const navigate = useNavigate();
   const { order, key } = location.state;
-  console.log(order);
-  console.log(key);
+
+  const handleDelete = () => {
+    let deletedOrders = JSON.parse(localStorage.getItem("deletedOrders")) || [];
+    deletedOrders.push(key);
+    localStorage.setItem("deletedOrders", JSON.stringify(deletedOrders));
+    navigate(-1);
+  };
 
   return (
     <div>
@@ -16,6 +22,7 @@ function ViewOrder() {
           </div>
         );
       })}
+      <button onClick={handleDelete}>Delete order</button>
     </div>
   );
 }
