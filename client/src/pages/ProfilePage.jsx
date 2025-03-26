2239fef23f68da19d0189e738e2c82f187ed4c70
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-03-26 16:15:56 +0700

    Mock data

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,5 +1,23 @@
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const handleOnClickEditBtn = () => {
+    console.log("Edit profile");
+  };
+
+  return (
+    <section>
+      <div>
+        <h1>Profile</h1>
+        <p>Username: oshinIsSoCutee</p>
+        <p>First Name: Shi</p>
+        <p>Last Name: JaoShi</p>
+        <p>Tel: 0984876577</p>
+        <p>Email: oshin.ganjanapas@gmail.com</p>
+        <p>Allergy: -</p>
+        <p>Birthday: 2001-11-10</p>
+      </div>
+      <button onClick={handleOnClickEditBtn}>Edit</button>
+    </section>
+  );
 }
 
 export default ProfilePage;
