2e612ca91de446f552730e9b01a09d12111eefb2
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-02 19:00:48 +0700

    Style Home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,25 +3,28 @@ import { useNavigate } from "react-router-dom";
 function HomePage() {
   const navigate = useNavigate();
 
-  const handleMenuBtn = () => {
-    navigate("/menu");
-  };
-
-  const handleHistoryBtn = () => {
-    navigate("/order-history");
+  const handleButtons = (page) => {
+    navigate(`/${page}`);
   };
 
   return (
-    <div>
-      <header>
-        <h1>MINIREST</h1>
-        <p>Welcome to Minirest</p>
+    <section className="flex flex-col items-center h-screen">
+      <header className="flex flex-col items-center">
+        <h1 className="text-black  uppercase text-[86px] font-['playfairDisplay'] mt-20">
+          MINIREST
+        </h1>
+        <p className="font-['caveat'] text-2xl">
+          Welcome to the place where your favorite drink gives you a wonderful
+          day.
+        </p>
       </header>
-      <nav>
-        <button onClick={handleMenuBtn}>MENU</button>
-        <button onClick={handleHistoryBtn}>HISTORY</button>
+      <nav className="flex gap-16 font-playfairDisplay absolute bottom-0 mb-48">
+        <button onClick={() => handleButtons("home")}>Home</button>
+        <button onClick={() => handleButtons("profile")}>Profile</button>
+        <button onClick={() => handleButtons("menu")}>Menu</button>
+        <button onClick={() => handleButtons("order-history")}>History</button>
       </nav>
-    </div>
+    </section>
   );
 }
 
