8a158d54e0becf8eaf290b43ae0e6e6708ee1462
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-02 19:15:40 +0700

    Add NotFoundPage

--- client/src/pages/UnAuthenticatedApp.jsx
+++ client/src/pages/UnAuthenticatedApp.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./LoginPage";
+import NotFoundPage from "./NotFoundPage";
 
 function UnAuthenticatedApp() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
