165dc083c63c9a1da1b9d541305d48ad088ba168
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-03 10:25:49 +0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      laptop: "1440px",
+    },
+    fontFamily: {
+      playfairDisplay: ["Playfair Display"],
+      roboto: ["Roboto"],
+      caveat: ["Caveat"],
+    },
+    extend: {
+      colors: {
+        text26: "#262626",
+        background: "#FFFAE9",
+        btnLogin: "#2E2828",
+        placeholder: "#989898",
+      },
+    },
   },
   plugins: [],
-}
-
+};
