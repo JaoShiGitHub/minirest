7ab249c9895401b2bd972ace98d734e10473df9d
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-03 16:23:14 +0700

    Update NotFoundPage.jsx

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -2,13 +2,18 @@ import { useNavigate } from "react-router-dom";
 
 function NotFoundPage() {
   const navigate = useNavigate();
+  
+  const handleButtons = () => {
+    navigate("/login");
+  };
+  
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <strong className="text-8xl ">404</strong>
       <p className="text-gray-500">Oh no.. page not found {`:(`}</p>
       <button
         className="bg-slate-900 text-gray-200 border-[0.5px] rounded-md mt-9 w-full h-8 max-w-48 border-black"
-        onClick={() => navigate("/login")}
+        onClick={handleButtons}
       >
         Login
       </button>
