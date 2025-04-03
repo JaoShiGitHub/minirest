dcea2c08fa1d35628933e54f38333dea331e6904
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-03 09:36:31 +0700

    Update order.js

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -2,6 +2,7 @@ import { pool } from "../utils/db.js";
 
 const orderHistory = async (req, res) => {
   const { customer_id } = req.query;
+
   try {
     const data = await pool.query(
       "SELECT * FROM customer_orders WHERE customer_id = $1",
@@ -9,20 +10,36 @@ const orderHistory = async (req, res) => {
     );
     const arrOrderId = data.rows.map((obj) => obj["order_id"]);
 
+    const idAndStatus = data.rows.map((obj) => ({
+      order_id: obj["order_id"],
+      status: obj["status"],
+    }));
+
     const placeholders = arrOrderId
       .map((_, index) => `$${index + 1}`)
       .join(", ");
+
+    console.log(placeholders);
 
     const orderItems = await pool.query(
       `SELECT * FROM order_items WHERE order_id IN (${placeholders})`,
       arrOrderId
     );
 
+    // I use this code because I want to group the items that have the same order_id together
+    // Otherwise they'll come separately
     const items = orderItems.rows.reduce((acc, item) => {
       acc[item.order_id] = acc[item.order_id] || [];
       acc[item.order_id].push(item);
       return acc;
     }, {});
+
+    const ordersAndStatus = Object.entries(items).map(([key, value]) => {
+      const orderId = key;
+      const items = value;
+
+      return { orderId, items };
+    });
 
     return res
       .status(200)
