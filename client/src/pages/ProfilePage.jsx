3fb9685cc8edf04651d63fa068c0d1a57575fa09
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-05 16:49:22 +0700

    Style profile

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -43,49 +43,86 @@ function ProfilePage() {
     }
   };
 
+  const ProfileCard = (props) => {
+    return (
+      <main className="bg-[#FDFDFA] w-full rounded-2xl shadow-lg px-32 py-16 mt-14">
+        {props.children}
+      </main>
+    );
+  };
+
   return (
-    <section>
+    <div className="w-full h-screen flex items-center justify-center">
       <NavBar />
-      {isClicked ? (
-        <form onSubmit={handleOnSubmit}>
-          <label htmlFor="username">Username </label>
-          <input type="text" name="username" id="username" />
-          <br />
-          <label htmlFor="first-name">First Name </label>
-          <input type="text" name="first-name" id="first-name" />
-          <br />
-          <label htmlFor="last-name">Last Name </label>
-          <input type="text" name="last-name" id="last-name" />
-          <br />
-          <label htmlFor="tel">Tel </label>
-          <input type="text" name="tel" id="tel" />
-          <br />
-          <label htmlFor="email">Email </label>
-          <input type="email" name="email" id="email" />
-          <br />
-          <label htmlFor="allergy">Allergy </label>
-          <input type="text" name="allergy" id="allergy" />
-          <br />
-          <label htmlFor="birthday">Birthday </label>
-          <input type="date" name="birthday" id="birthday" />
-          <br />
-          <br />
+      <section className="flex flex-col w-full items-center max-w-[1180px] gap-6">
+        {isClicked ? (
+          <ProfileCard>
+            <form onSubmit={handleOnSubmit}>
+              <label htmlFor="username">Username </label>
+              <input type="text" name="username" id="username" />
+              <br />
+              <label htmlFor="first-name">First Name </label>
+              <input type="text" name="first-name" id="first-name" />
+              <br />
+              <label htmlFor="last-name">Last Name </label>
+              <input type="text" name="last-name" id="last-name" />
+              <br />
+              <label htmlFor="tel">Tel </label>
+              <input type="text" name="tel" id="tel" />
+              <br />
+              <label htmlFor="email">Email </label>
+              <input type="email" name="email" id="email" />
+              <br />
+              <label htmlFor="allergy">Allergy </label>
+              <input type="text" name="allergy" id="allergy" />
+              <br />
+              <label htmlFor="birthday">Birthday </label>
+              <input type="date" name="birthday" id="birthday" />
+              <br />
+              <br />
+            </form>
+          </ProfileCard>
+        ) : (
+          <ProfileCard>
+            <div className="flex justify-evenly gap-x-40">
+              <section className="flex flex-col items-center gap-y-4">
+                <div className="w-[300px] h-[300px] bg-amber-800 rounded-[35px]">
+                  IMAGE
+                </div>
+                <b>oshinIsSoCutee</b>
+              </section>
+              <section className="flex flex-col gap-y-3">
+                <p>First Name: Shi</p>
+                <p>Last Name: JaoShi</p>
+                <p>Tel: 0984876577</p>
+                <p>Birthday: 2001-11-10</p>
+                <p>Email: oshin.ganjanapas@gmail.com</p>
+                <p>Allergy: -</p>
+              </section>
+            </div>
+          </ProfileCard>
+        )}
+
+        {isClicked ? (
           <button type="submit">SUBMIT</button>
-        </form>
-      ) : (
-        <div>
-          <p>Username: oshinIsSoCutee</p>
-          <p>First Name: Shi</p>
-          <p>Last Name: JaoShi</p>
-          <p>Tel: 0984876577</p>
-          <p>Email: oshin.ganjanapas@gmail.com</p>
-          <p>Allergy: -</p>
-          <p>Birthday: 2001-11-10</p>
-          <button onClick={handleOnClickEditBtn}>Edit</button>
-        </div>
-      )}
-    </section>
+        ) : (
+          <button
+            className="self-end bg-white shadow-lg w-28 h-10 rounded-[50px] font-bold"
+            onClick={handleOnClickEditBtn}
+          >
+            Edit
+          </button>
+        )}
+      </section>
+    </div>
   );
 }
 
 export default ProfilePage;
+
+// <p>First Name: Shi</p>
+// <p>Last Name: JaoShi</p>
+// <p>Tel: 0984876577</p>
+// <p>Birthday: 2001-11-10</p>
+// <p>Email: oshin.ganjanapas@gmail.com</p>
+// <p>Allergy: -</p>
