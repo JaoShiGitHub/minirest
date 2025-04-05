7db9630b8fab7825cf5b115d3f352ba03dd5fece
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-05 16:50:15 +0700

    Update NavBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,14 +3,14 @@ import ButtonHomePage from "./ButtonHomePage";
 function NavBar() {
   const pages = ["Home", "Profile", "Menu", "History"];
   return (
-    <section className="flex bg-white justify-between px-20 py-5">
+    <nav className="absolute top-0 flex w-full bg-white justify-between px-20 py-5">
       <h1 className="font-playfairDisplay text-2xl font-bold">MINIREST</h1>
       <div className="max-w-lg w-full font-playfairDisplay flex justify-between">
         {pages.map((page) => {
           return <ButtonHomePage key={page} page={page} />;
         })}
       </div>
-    </section>
+    </nav>
   );
 }
 
