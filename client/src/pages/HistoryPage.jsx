1dea14615cca6a75b7084da3ab5c62845874273c
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-05 12:27:25 +0700

    Update HistoryPage.jsx

--- client/src/pages/HistoryPage.jsx
+++ client/src/pages/HistoryPage.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 function HistoryPage() {
   const [orders, setOrders] = useState([]);
-  const [deletedOrders, setDeletedOrders] = useState(
-    JSON.parse(localStorage.getItem("deletedOrders")) || []
-  );
+  // const [deletedOrders, setDeletedOrders] = useState(
+  //   JSON.parse(localStorage.getItem("deletedOrders")) || []
+  // );
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const style = {
     cursor: "pointer",
@@ -18,61 +18,46 @@ function HistoryPage() {
     getOrder();
   }, []);
 
-  useEffect(() => {
-    const storedDeletedOrders =
-      JSON.parse(localStorage.getItem("deletedOrders")) || [];
-    setDeletedOrders(storedDeletedOrders);
-  }, []);
+  // useEffect(() => {
+  //   const storedDeletedOrders =
+  //     JSON.parse(localStorage.getItem("deletedOrders")) || [];
+  //   setDeletedOrders(storedDeletedOrders);
+  // }, []);
 
   const getOrder = async () => {
     try {
       const orders = await axios.get(
         "http://localhost:4000/order/history?customer_id=10"
       );
-      setOrders(orders.data.items);
+      console.log(orders?.data?.filteredOrdered);
+
+      setOrders(orders?.data?.filteredOrdered);
     } catch (err) {
       alert(err.response.data.msg);
     }
   };
 
-  const handleOrderClick = (key, order) => {
-    navigate("/view-order", {
-      state: { order, key },
-    });
-  };
+  // const handleOrderClick = (key, order) => {
+  //   navigate("/view-order", {
+  //     state: { order, key },
+  //   });
+  // };
 
   return (
-    <div>
-      <h1>HistoryPage</h1>
-      {Object.entries(orders)
-        .filter(([key]) => !deletedOrders.includes(key))
-        .map(([key, order]) => {
+    <div className="h-screen">
+      <h1 className="">HistoryPage</h1>
+      <ul>
+        {orders.map((item) => {
           return (
-            <button
-              style={style}
-              key={key}
-              onClick={() => handleOrderClick(key, order)}
-            >
-              <h1>Order: {key}</h1>
-              <div>
-                {order.map((item) => {
-                  return (
-                    <div key={item.id}>
-                      <div>{item.product_name}</div>
-                      <span>{item.product_price}</span>
-                    </div>
-                  );
-                })}
-              </div>
-              <span>
-                Total:
-                {order.reduce((orderAcc, item) => {
-                  return orderAcc + parseFloat(item.product_price);
-                }, 0)}
-              </span>
-            </button>
+            <li className="bg-white mb-5" key={item.order_id}>
+              <span>Oder: {item.order_id}</span>
+              <p>Status: {item.status}</p>
+              <p>{item.items.length} Items</p>
+              <button className="mt-5 underline">See more</button>
+            </li>
           );
         })}
+      </ul>
     </div>
   );
 }
