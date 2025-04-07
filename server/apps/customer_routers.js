2541b46d41b12e5e359f86d80dd86e0161d8a70e
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-07 14:35:08 +0700

    Add info route

--- server/apps/customer_routers.js
+++ server/apps/customer_routers.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { customerRegister, customerOrder } from "../controllers/customers.js";
+import {
+  customerRegister,
+  customerOrder,
+  customerInfo,
+} from "../controllers/customers.js";
 import { validateRegisterCustomer } from "./customerValidation.js";
 
 const customer = Router();
 
 customer.post("/register", validateRegisterCustomer, customerRegister);
 customer.post("/order", customerOrder);
+customer.get("/customer", customerOrder);
+customer.get("/info", customerInfo);
 
 export default customer;
