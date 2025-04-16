d338e0b5d700eca69599b4b824dcc96a965476fc
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-16 14:07:58 +0700

    Add Lato font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -10,6 +10,7 @@ module.exports = {
       playfairDisplay: ["Playfair Display"],
       roboto: ["Roboto"],
       caveat: ["Caveat"],
+      lato: ["Lato"],
     },
     extend: {
       colors: {
