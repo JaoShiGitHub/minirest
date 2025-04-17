b7a39cf3b099d79db98d445ff12e6bebea1142f5
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-17 13:46:55 +0700

    Update UI

--- client/src/components/FullHistory.jsx
+++ client/src/components/FullHistory.jsx
@@ -1,29 +1,44 @@
 function FullHistory() {
   return (
-    <main className="bg-pink-100 h-screen w-screen bg-[rgba(0,0,0,0.55)] absolute top-0 flex justify-center items-center">
-      <div className="max-w-[550px]">
-        <section className="bg-white py-20 px-10 rounded-3xl shadow-xl">
-          <div className="flex justify-between items-end">
+    <main className="h-screen w-screen bg-[rgba(0,0,0,0.55)] top-0 flex justify-center items-center fixed ">
+      <div className="min-w-[450px]">
+        <section className="bg-white pt-20 pb-20 px-10 rounded-3xl shadow-xl">
+          <div className="flex justify-between items-end mb-8">
             <b className="text-3xl">nkqrfw7ygad</b>
-            <p className="text-lg">Status: Order Placed</p>
+            <p className="">Status: Order Placed</p>
           </div>
-          <div>
+          <div className="flex flex-col gap-y-3 pl-9">
             <p>Date: 3/6/2025</p>
             <p>Dining : Delivery</p>
             <p>Payment: mobile babanking </p>
             <p>Request : 100% Sweet please</p>
-            <hr />
           </div>
-          <b>3 Items</b>
-          <div>
-            <span>Thai Tea</span>
+          <hr className="my-8" />
+          <b className="text-lg">3 Items</b>
+          <div className="flex flex-col gap-2 mt-4 mb-6">
+            <div className="flex justify-between px-10">
+              <span>Thai Tea</span>
+              <span>55.00</span>
+            </div>
+            <div className="flex justify-between px-10">
+              <span>Butterfly Pea Lemonade</span>
+              <span>45.00</span>
+            </div>
+            <div className="flex justify-between px-10">
+              <span>Cha Manao</span>
+              <span>45.00</span>
+            </div>
+          </div>
+          <div className="flex justify-between px-10">
+            <strong>Total</strong>
+            <span>145.00</span>
           </div>
         </section>
-        <section className="text-white">
-          <button className="bg-[#010617] text-2xl shadow-xl px-20 py-5 rounded-full">
+        <section className="text-white mt-9 flex justify-evenly">
+          <button className="bg-[#010617] text-2xl shadow-xl min-w-[180px] py-3 rounded-full">
             Close
           </button>
-          <button className=" bg-[#D90000] text-2xl shadow-xl px-20 py-5 rounded-full">
+          <button className=" bg-[#D90000] text-2xl shadow-xl min-w-[180px] py-3 rounded-full">
             Delete
           </button>
         </section>
