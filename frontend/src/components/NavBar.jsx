ac710c64ed6a877fa527f1bc5b5d965a38c876a5
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-17 12:40:52 +0700

    Adjust NavBar css

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import ButtonHomePage from "./ButtonHomePage";
 function NavBar() {
   const pages = ["Home", "Profile", "Menu", "History"];
   return (
-    <nav className="flex w-full bg-white justify-between px-20 py-5">
-      <h1 className="font-playfairDisplay text-2xl font-bold">MINIREST</h1>
-      <div className="max-w-lg w-full font-playfairDisplay flex justify-between">
+    <nav className="flex w-full bg-[#FDFDFA] justify-between px-36 py-8">
+      <h1 className="font-playfairDisplay text-3xl font-bold">MINIREST</h1>
+      <div className="gap-x-14 max-w-[500px] text-xl w-full font-playfairDisplay flex justify-between">
         {pages.map((page) => {
           return <ButtonHomePage key={page} page={page} />;
         })}
