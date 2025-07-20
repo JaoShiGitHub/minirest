9344d47bc0f319d64f81c9ea5fd2c0c06de3f0f0
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-20 17:40:21 +0700

    Finish style form

--- frontend/src/components/ConfirmOrder.jsx
+++ frontend/src/components/ConfirmOrder.jsx
@@ -1,16 +1,19 @@
 function ConfirmOrder(props) {
   const { items } = props;
+
   const handleConfirm = async () => {};
   console.log(items);
 
   const total = items.reduce((acc, item) => acc + item.price * item.count, 0);
+  const order_items = items.reduce((acc, item) => acc + item.count, 0);
+
   return (
-    <section className="fixed overflow-hidden inset-0 bg-[hsla(0,0%,10%,0.6)] flex items-center justify-center w-screen h-screen">
+    <section className="tracking-wide fixed flex-col overflow-hidden gap-y-5 inset-0 bg-[hsla(0,0%,10%,0.6)] flex items-center justify-center w-screen h-screen">
       <div className="bg-white min-w-[450px] rounded-2xl p-10">
-        <h2>
-          {items.length > 1 ? "Items" : "Item"}: {items.length}
+        <h2 className="text-2xl font-bold">
+          {order_items} {order_items > 1 ? "Items" : "Item"}
         </h2>
-        <ul>
+        <ul className="flex flex-col gap-y-1 mt-4">
           {items.map((item) => (
             <li key={item.menu_id} className="flex justify-between">
               <div className="flex gap-x-4">
@@ -20,12 +23,32 @@ function ConfirmOrder(props) {
               <span>{item.price} ฿</span>
             </li>
           ))}
-          <div className="flex justify-between">
+          <div className="flex justify-between mt-4">
             <b>Total</b>
             <span>{total}</span>
           </div>
+          <label for="dining" className="flex flex-col items-center">
+            <p className=""> Dining Option:</p>
+            <select
+              id="dining"
+              name="dining"
+              className="appearance-none bg-neutral-200 shadow-lg w-full py-1 px-4 mb-4 mt-2 rounded-lg outline-none hover:cursor-pointer"
+              // className="w-full hover:cursor-pointer px-4 py-1 rounded-lg  pr-10 text-gray-700  focus:outline-none"
+            >
+              <option value="dine-in">Dine In</option>
+              <option value="takeaway">Takeaway</option>
+              <option value="delivery">Delivery</option>
+            </select>
+          </label>
         </ul>
+        <div className="flex items-center flex-col gap-y-2">
+          <p>Note to us:</p>
+          <textarea className="min-h-40 w-full px-4 py-3 border border-slate-300 rounded-lg shadow-lg outline-none"></textarea>
+        </div>
       </div>
+      <button className=" font-medium hover:bg-orange-800 bg-orange-200 hover:text-amber-50 text-black min-w-[450px] rounded-xl text-xl  py-2">
+        Confirm Order
+      </button>
     </section>
   );
 }
