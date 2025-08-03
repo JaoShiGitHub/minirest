31bf5fc9b255603ea2423def4c48eba045c1f65c
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-08-03 23:53:23 +0700

    Update status code

--- backend/controllers/customers.js
+++ backend/controllers/customers.js
@@ -8,55 +8,64 @@ const customerLogin = async (req, res) => {
   const { identifier, password } = req.body;
 
   const type = isEmail(identifier) ? "Email" : "Username";
+  try {
+    const data = await pool.query(
+      `SELECT * FROM customers WHERE ${type.toLowerCase()} = $1`,
+      [identifier]
+    );
 
-  const data = await pool.query(
-    `SELECT * FROM customers WHERE ${type.toLowerCase()} = $1`,
-    [identifier]
-  );
+    const customer = data.rows[0];
 
-  const customer = data.rows[0];
-
-  if (customer.length === 0) {
-    return res.status(404).json({ message: `${type} not found` });
-  }
-
-  const isValidPassword = await bcrypt.compare(password, customer.password);
-
-  if (!isValidPassword) {
-    return res.status(400).json({ message: `Invalid Password` });
-  }
-
-  const token = jwt.sign(
-    {
-      id: customer.id,
-      username: customer.username,
-      firstName: customer.firstname,
-    },
-    process.env.SECRET_KEY,
-    {
-      expiresIn: "1d",
+    if (customer.length === 0) {
+      return res.status(404).json({ message: `${type} not found` });
     }
-  );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "Strict",
-    maxAge: 24 * 60 * 60 * 1000,
-  });
+    const isValidPassword = await bcrypt.compare(password, customer.password);
 
-  return res.status(200).json({ message: "Login Successfully" });
+    if (!isValidPassword) {
+      return res.status(400).json({ message: `Invalid Password` });
+    }
+
+    const token = jwt.sign(
+      {
+        id: customer.id,
+        username: customer.username,
+        firstName: customer.firstname,
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Logged in successfully." });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // Logout
 const customerLogout = (req, res) => {
-  res.clearCookie("token", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "Strict",
-  });
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+    });
 
-  return res.status(200).json({ message: "Logged out successfully" });
+    return res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // Register
