1ca73a6ba8f7c7d231b9ca1fec7476c6c010c754
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-08-03 23:57:42 +0700

    Protect routes

--- frontend/src/pages/AuthenticatedApp.jsx
+++ frontend/src/pages/AuthenticatedApp.jsx
@@ -1,30 +1,65 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./HomePage";
 import MenuPage from "./MenuPage";
 import NotFoundPage from "./NotFoundPage";
-import PlaceOrderPage from "./PlaceOrderPage";
-
 import OrderReceivedPage from "./OrderReceivedPage";
-// import ViewOrder from "./ViewOrder";
 import { HistoryPage } from "./HistoryPage";
 import ProfilePage from "./ProfilePage";
-import InsertMenu from "./InsertMenuPage";
+import ProtectedRoute from "./ProtectedRoute";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
 function AuthenticatedApp() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/order-now" element={<PlaceOrderPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route path="/order-received" element={<OrderReceivedPage />} />
-        {/* <Route path="/view-order" element={<ViewOrder />} /> */}
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/not-found" element={<NotFoundPage />} />
-        <Route path="/insert-menu" element={<InsertMenu />} />
 
-        <Route path="*" element={<NotFoundPage />} />
+        {/* Protected routes */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/menu"
+          element={
+            <ProtectedRoute>
+              <MenuPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <HistoryPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/order-received"
+          element={
+            <ProtectedRoute>
+              <OrderReceivedPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </div>
   );
