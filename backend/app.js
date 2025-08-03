6b101b60f7e7c6c6984ec9ebe8f204baaed68f09
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-08-04 00:22:27 +0700

    Create /checkme route to verify if the user is still logged in

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ dotenv.config();
 import menu from "./routers/menu_routes.js";
 import customer from "./routers/customer_routes.js";
 import order from "./routers/order_routes.js";
+import authUser from "./middlewares/auth.js";
 
 app.use(express.json({ limit: "10mb" })); // allows Express to read and understand JSON data sent in the body of a request.
 app.use(cookieParser());
@@ -21,6 +22,18 @@ app.use(
 app.use("/customer", customer);
 app.use("/menu", menu);
 app.use("/order", order);
+
+app.get("/checkme", authUser, (req, res) => {
+  try {
+    return res.status(200).json({
+      success: true,
+      message: "Logged in successfully.",
+      user: req.customer,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+});
 
 app.listen(4000, () => {
   console.log("Server is listening at port 4000");
